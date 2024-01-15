0af6788de677eddda84d11117765c6a2cea1f92d
Your Name <you@example.com>
2024-01-15 13:36:55 +0300

    2.1

--- js/script.js
+++ js/script.js
@@ -2,22 +2,17 @@ $(document).ready(function() {
     $('.solution_slider').slick({
         slidesToShow: 1,
         dots: true,
-        dotsClass: "slider_dots",
+        dotsClass: 'slider_dots',
         arrows: true,
         prevArrow: '<div class="slider_arrow slider_arrowleft"></div>',
         nextArrow: '<div class="slider_arrow slider_arrowright"></div>',
     });
 
-    $('.menu-item:first-child').on('mouseenter', function() {
-        $('.sub-menu').css({
-            'display': 'block'
-        });
-    });
-    $('.header').on("mouseleave", function() {
-        $('.sub-menu').css({
-            'display': 'none'
-        });
-    });
+    $('.menu-item:first-child').on( 'mouseenter', function() {
+        $('.sub-menu').addClass('sub-menu-active');
+    } ).on( "mouseleave", function() {
+        $('.sub-menu').removeClass('sub-menu-active');
+    } );
 
     if ($(window).width() > 992) {
         $('.burger_menu').css('display', 'none');
@@ -35,9 +30,8 @@ $(document).ready(function() {
     } else {
         $('.burger_menu').css('display', 'block');
     }
-
+    
     $(function() {
         $('.main_card').matchHeight();
     });
-    
 });
